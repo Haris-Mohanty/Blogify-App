9eb4943071f887e6823673896c1e50dfce59f55a
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-20 19:31:10 +0530

    routes- companyRoutes.js- Create private api code processing....

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 const indexRoutes = require("./routes/indexRoutes");
 const signupRouter = require("./routes/signupRoutes");
+const companyRoutes = require("./routes/companyRoutes");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRoutes);
 app.use("/api/signup", signupRouter);
+app.use("/api/private/company", companyRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
