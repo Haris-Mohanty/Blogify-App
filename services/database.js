45f52a0c71132e5bcee1bd2137e977bde7c64e7d
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-22 20:07:51 +0530

    services- database.js- Code for database connect and use fixed.

--- services/database.js
+++ services/database.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
 const companySchema = require("../model/companyModel");
+
+const url = "mongodb://127.0.0.1:27017/blogify";
+mongoose.connect(url);
+
+const createRecord = async (data) => {
+  const dataRes = await new companySchema(data).save();
+};
+
+module.exports = {
+  createRecord: createRecord,
+};
