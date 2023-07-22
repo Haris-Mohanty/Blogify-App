d7ed17f61a51d48d27d92e7e23acb724c5379142
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-22 19:09:01 +0530

    services- httpService.js- Code processing..

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -2,17 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 
-const superTest = require("supertest");
-const jwt = require("jsonwebtoken");
-
-const secretKey = process.env.SECRET_KEY;
-
 const tokenService = require("../services/tokenService");
+const httpService = require("../services/httpService");
 
 router.post("/", async (req, res) => {
   let expiresIn = 120;
   const token = await tokenService.createToken(req, expiresIn);
-  console.log(token);
+  httpService.postRequest(token);
 });
 
 module.exports = router;
---
+++ services/httpService.js
@@ -0,0 +1,7 @@
+const ajax = require("supertest");
+
+const postRequest = (token) => {
+console.log(token)
+};
+
+module.exports = { postRequest: postRequest };
