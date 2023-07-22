f85d60cd2dcf79524465da8b9172e146bd05c1ce
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-22 20:01:21 +0530

    model- companyModel.js- Create schema created.

--- model/companyModel.js
+++ model/companyModel.js
@@ -1,2 +1,25 @@
 const mongoose = require("mongoose");
 
+const companySchema = new mongoose.Schema({
+  company: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+  },
+  mobile: Number,
+  emailVerified: {
+    type: Boolean,
+  },
+  password: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//****** EXPORT *****/
+module.exports = mongoose.model("Inventory", companySchema);
