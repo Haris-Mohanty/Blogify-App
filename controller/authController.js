bf208fb975c7fbcc789989bb11efb6618cd233c6
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-02 10:08:50 +0530

    controller- authController.js- Data get from database successfully.

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,17 @@
 const tokenService = require("../services/tokenService");
+const dataBase = require("../services/database");
 const checkUserLogged = async (req) => {
   const tokenData = tokenService.verifyToken(req);
-  if(tokenData.isVerified){
-    
+  if (tokenData.isVerified) {
+    const query = {
+      token: req.cookies.authToken,
+      islogged: true,
+    };
+
+    const userData = await dataBase.getRecordByQuery(query, "userSchema");
+    console.log(userData);
+  } else {
+    return false;
   }
 };
 
