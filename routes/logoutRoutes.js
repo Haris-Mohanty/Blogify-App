c3b36f92c7e985543a1baefa62a50da23e7eabc3
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-04 10:26:53 +0530

    routes- logoutRoutes.js- Create routes for logout code fixed.

--- routes/logoutRoutes.js
+++ routes/logoutRoutes.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//create routes || end point
+router.get("/", (req, res) => {
+  res.json("Logout REquested!");
+});
+
 module.exports = router;
