741c0fdfe149b9c4ed264e245e40979c6cf68409
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-10 20:16:28 +0530

    Profile page- model- studentsModel.js- Schema created successfully.

--- model/studentsModel.js
+++ model/studentsModel.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-    
+  companyId: String,
+  studentName: String,
+  studentEmail: {
+    type: String,
+    unique: true,
+  },
+  studentFather: String,
+  studentDob: String,
+  studentMobile: Number,
+  studentCountry: String,
+  studentState: String,
+  studentPin: String,
+  studentAddress: String,
 });
 
 module.exports = mongoose.model("student", studentSchema);
