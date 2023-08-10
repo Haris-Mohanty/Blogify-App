f5a0307c2149e0446649b704cb95a8e016095689
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-10 20:30:08 +0530

    Profile page- controller- studentsController.js- Verify token code fixed.

--- controller/studentsController.js
+++ controller/studentsController.js
@@ -2,8 +2,15 @@ const tokenService = require("../services/tokenService");
 const dataBase = require("../services/database");
 
 const create = (req, res) => {
-    //Token Verify
-    const tokenData = toke
+  //Token Verify
+  const tokenData = tokenService.verifyToken(req);
+  if (tokenData.isVerified) {
+    
+  } else {
+    res.status(401).json({
+      message: "Permission Denied!",
+    });
+  }
 };
 
 module.exports = {
