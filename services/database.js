4352daf4fa5ab76d422faa22c9e649feee07a173
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-13 22:15:48 +0530

    Profile page- serivices- database.js- Paginate code added.

--- services/database.js
+++ services/database.js
@@ -40,7 +40,7 @@ const countData = async (schema) => {
 //****** PAGINATE (SHOW STUDENT DETAILS) ********/
 const paginateData = async (from, to, schema) => {
   const currentSchema = schemaList[schema];
-  const dataRes = await currentSchema.find().limit(4);
+  const dataRes = await currentSchema.find().skip(from).limit(to);
   return dataRes;
 };
 
