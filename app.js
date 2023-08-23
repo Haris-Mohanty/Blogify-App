929f2de59ec33ecca25494a1986f2a818b77d70f
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-23 12:34:01 +0530

    Profile page- app.js- Middle ware routes added successfully.

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const profileRouter = require("./routes/profileRoutes");
 const authController = require("./controller/authController");
 const logoutRoutes = require("./routes/logoutRoutes");
 const studentsRoutes = require("./routes/studentsRoutes");
+const sendMailRoutes = require("./routes/sendMailRoutes");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -67,6 +68,7 @@ app.use("/api/private/user", userRouter);
 app.use("/logout", logoutRoutes);
 app.use("/students", studentsRoutes);
 app.use("/profile", authLogger(), profileRouter);
+app.use("/sendMail", sendMailRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
