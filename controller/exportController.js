7b23e379e220aa78fe27822768f9294f17333d7c
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-01 12:27:44 +0530

    Profile page- controller- exportController- Export student to pdf code processing.....

--- controller/exportController.js
+++ controller/exportController.js
@@ -1,4 +1,7 @@
-const pdf = (req, res) => {};
+const tokenService = require("../services/tokenService");
+const pdf = (req, res) => {
+  let token = tokenService.verifyToken(req);
+};
 
 module.exports = {
   pdf: pdf,
