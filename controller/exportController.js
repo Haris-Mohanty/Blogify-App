616f42896a38a99a7bbcfd532e0a77c41bda6425
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-01 12:34:15 +0530

    Profile page- controller- exportController- Create pdf and data in pdf code processing.....

--- controller/exportController.js
+++ controller/exportController.js
@@ -1,7 +1,24 @@
 const tokenService = require("../services/tokenService");
+const pdfKit = require("pdfkit-table");
+const fs = require("fs");
+
 const pdf = async (req, res) => {
   let token = await tokenService.verifyToken(req);
   if (token.isVerified) {
+    //Create pdf
+    const doc = new pdfKit({
+      margin: 30,
+      page: "A4",
+    });
+
+    //path
+    doc.pipe(fs.createWriteStream("public/exports/new.pdf"));
+
+    //Data in pdf
+    doc.text("Students Data!");
+
+    //End pdfkit
+    doc.end()
   } else {
     res.status(401).json({
       message: "Permission Denied at PDF!",
